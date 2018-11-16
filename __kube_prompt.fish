d618c85d1fe9bde5f5cd26453bc2d65e6ed2f1a0
Yurii Polishchuk <yurii.polishchuk@finsight.com>
2018-11-16 13:12:09 +0200

    fix bug with caching

--- __kube_prompt.fish
+++ __kube_prompt.fish
@@ -36,7 +36,7 @@ function __kube_ps_update_cache
 
   for conf in (string split ':' "$kubeconfig")
     if /bin/test -r "$conf"
-      if /bin/test -z "$__kube_ps_timestamp"; or /bin/test (/usr/bin/stat -f '%m' "$conf") -gt "$__kube_ps_timestamp"
+      if /bin/test -z "$__kube_ps_timestamp"; or /bin/test (/usr/bin/stat -f '%m' "$conf") -lt "$__kube_ps_timestamp"
         __kube_ps_cache_context
         __kube_ps_cache_namespace
         set -g __kube_ps_kubeconfig "$kubeconfig"
